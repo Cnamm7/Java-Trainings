1fc1289bc2d1b9f049b37e394f3162a9dd56b359
Cnamm7 <Cna.mm7@gmail.com>
2023-10-13 10:25:25 -0400

    implemented Volatile learning

---
+++ Java-Training/src/main/java/VolatileExercise/VolatileData.java
@@ -0,0 +1,13 @@
+package VolatileExercise;
+
+public class VolatileData {
+    private volatile int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void increaseCounter() {
+        ++counter;
+    }
+}
--- Java-Training/src/main/java/main/Main.java
+++ Java-Training/src/main/java/main/Main.java
@@ -1,7 +1,29 @@
 package main;
 
+import VolatileExercise.VolatileData;
+import VolatileExercise.VolatileThread;
+
+import java.lang.System;
 public class Main {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws InterruptedException{
+        runVolatile();
+    }
+
+    public static void runVolatile() throws InterruptedException{
+        int noOfThreads = 2;
+        VolatileData volatileData = new VolatileData();
+        Thread[] threads = new Thread[noOfThreads];
+        for (int i = 0; i < noOfThreads; ++i) {
+            threads[i] = new VolatileThread(volatileData);
+        }
+        for (int i = 0; i < noOfThreads; ++i) {
+            threads[i].start();;
+        }
+        for (int i = 0; i < noOfThreads; ++i) {
+            threads[i].join();
+        }
     }
 }
+
+
